c00ff2b650c94f75457afdcb9ef6426f21c2f76c
Sergey Shtukin <sery.sht@gmail.com>
2018-07-22 23:43:56 +0300

    working with UI

--- app/src/main/java/com/serysht/todolist/CreateDialogFragment.java
+++ app/src/main/java/com/serysht/todolist/CreateDialogFragment.java
@@ -1,67 +1,70 @@
 package com.serysht.todolist;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class CreateDialogFragment extends DialogFragment {
     public static final String TAG = "CreateDialogFragment";
-    private static final int REQUEST_CODE = 1;
     public static final String EXTRA_DATE = "date";
+    private static final int REQUEST_CODE = 1;
+    private static final String TOAST_ADD_TITLE = "You should add title";
 
     private EditText mTitle;
     private EditText mAdditional;
     private Button mSaveButton;
-    private TaskManager mTaskManager;
-    private ImageButton mDateButton;
+    private Button mDateButton;
     private Task mTask;
-    private TextView mDate;
+    private TaskManager mTaskManager;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_create,
                 container, false);
-        mTitle = view.findViewById(R.id.task_title);
-        mAdditional = view.findViewById(R.id.task_additional);
-        mSaveButton = view.findViewById(R.id.save_button);
-        mDateButton = view.findViewById(R.id.date_opened_button);
-        mDate = view.findViewById(R.id.date_text_view);
+
+        mTitle = view.findViewById(R.id.dialog_create_task_title);
+        mAdditional = view.findViewById(R.id.dialog_create_task_additional);
+        mSaveButton = view.findViewById(R.id.dialog_create_save_button);
+        mDateButton = view.findViewById(R.id.dialog_create_date_opened_button);
+
         mTaskManager = TaskManager.get(getContext());
         mTask = new Task();
 
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!mTitle.getText().toString().equals("")){
 
-                mTask.setTitle(mTitle.getText().toString());
-                mTask.setAdditional(mAdditional.getText().toString());
-                mTaskManager.addTask(mTask);
+                    mTask.setTitle(mTitle.getText().toString());
+                    mTask.setAdditional(mAdditional.getText().toString());
+                    mTaskManager.addTask(mTask);
 
-                Intent intent = new Intent();
-                if(getTargetFragment() == null){
+                    Intent intent = new Intent();
+                    if(getTargetFragment() == null){
+                        dismiss();
+                    }
+                    getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
                     dismiss();
                 }
-                getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
-                dismiss();
+                else {
+                    Toast.makeText(getContext(),
+                    TOAST_ADD_TITLE,
+                    Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -74,7 +77,6 @@ public class CreateDialogFragment extends DialogFragment {
                 dialog.show(manager, DatePickerFragment.TAG);
             }
         });
-
         return view;
     }
 
@@ -93,8 +95,7 @@ public class CreateDialogFragment extends DialogFragment {
                     .getSerializableExtra(EXTRA_DATE);
 
             mTask.setDate(date);
-            mTaskManager.updateTask(mTask);
-            mDate.setText(date.toString());
+            mDateButton.setText(DateFormat.getDateInstance().format(mTask.getDate()));
         }
     }
 }
